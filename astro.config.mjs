40ea0edb33456db803202817324d16de85f3cf77
muhhae <haekal.muhamat@gmail.com>
2024-03-11 22:25:33 +0700

    add disqus for comment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,17 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel/serverless";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()],
-  output: "server",
-  adapter: vercel()
+	site: "https://muhhae.vercel.app",
+	integrations: [mdx(), sitemap(), tailwind()],
+	output: "server",
+	adapter: vercel(),
+	server: {
+		port: 8080,
+	},
 });
